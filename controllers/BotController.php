d0cf350e2b55ddba0b24e07c81640011bf20903a
ShamanHead <ronyplay247@gmail.com>
2021-06-13 01:32:41 +0300

    r

---
+++ controllers/BotController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controller;
+
+require_once "vendor/autoload.php";
+
+use Colyt\Core as Core;
+
+//$route = $_GET['route'];
+
+use Model\BotModel as BotModel;
+
+$botModel = new BotModel('1867256706:AAHca7ts3DcGDFxF8iz0bB1euoTAJY4d1hY');
+
+$botModel->sendMessage('hello');
+
---
+++ models/BotModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Model;
+
+use \Telbot\Bot as Bot;
+use \Telbot\Inquiry as Inquiry;
+use \Telbot\InputHandle as InputHandle;
+
+class BotModel
+{
+
+    private $bot;
+
+    private $messagePresets = [
+      'hello' => 'Добро пожаловать!'
+    ];
+
+    private $keyboardPresets = [
+
+    ];
+
+    private $inlinePresets = [
+
+    ];
+
+    public function createBot(string $token){
+        $this->InputHandle = new InputHandle();
+        $this->bot = new Bot($token);
+        return true;
+    }
+
+    public function getBot(){
+        return $this->bot;
+    }
+
+    public function sendMessage($preset){
+        Inquiry::send($bot, 'sendMessage', [
+            'chat_id' => $this->InputHandle->getChatId(),
+            'text' => $this->messagePresets[$preset]
+
+        ]);
+    }
+
+}
