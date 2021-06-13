d8f7d269276a8d7f5857f6cda9357c9d3217f9b9
ShamanHead <ronyplay247@gmail.com>
2021-06-14 00:12:17 +0300

    r

--- Mapping/Entities.User.php
+++ Mapping/Entities.User.php
@@ -6,9 +6,7 @@ $metadata->mapField(array(
     'id' => true,
     'fieldName' => 'id',
     'type' => 'integer',
-    'options' => [
-        'GeneratedValue' => true
-    ]
+    'GeneratedValue' => true
 ));
 
 $metadata->mapField(array(
