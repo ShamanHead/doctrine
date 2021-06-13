ba37735363caeb69a4925127eab59c0f95bc678b
ShamanHead <ronyplay247@gmail.com>
2021-06-13 23:28:39 +0300

    r

---
+++ models/UserModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controller;
+
+require_once "config/bootstrap.php";
+
+class UserModel
+{
+
+    function __construct(string $userId){
+        $this->userId = $userId;
+        $repo = $entityManager->getRepository('User');
+        $this->instanse = $repo->findBy([
+            'userId' => $userId
+        ]);
+    }
+
+    public function setLanguage(string $lang){
+        $this->instanse->setLanguage($lang);
+    }
+
+    public function flush(){
+        $entityManager->flush();
+    }
+}
